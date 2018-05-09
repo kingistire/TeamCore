7b96acc9bce9795174d5e9abe9c82170cb0c8944
Jason Tam <jasontam2233@gmail.com>
2018-05-09 22:07:09 +1000

    Created functionality of buttons hiding and showing

--- project/prototype1/prototype1/Login.Designer.cs
+++ project/prototype1/prototype1/Login.Designer.cs
@@ -30,12 +30,28 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(540, 253);
+            this.button1.Location = new System.Drawing.Point(73, 207);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,20 +64,117 @@
             this.pictureBox1.ImageLocation = "";
             this.pictureBox1.Location = new System.Drawing.Point(39, 46);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(394, 368);
+            this.pictureBox1.Size = new System.Drawing.Size(161, 141);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ImageLocation = "";
+            this.pictureBox2.Location = new System.Drawing.Point(321, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.ImageLocation = "";
+            this.pictureBox3.Location = new System.Drawing.Point(534, 46);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.ImageLocation = "";
+            this.pictureBox4.Location = new System.Drawing.Point(756, 46);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.ImageLocation = "";
+            this.pictureBox5.Location = new System.Drawing.Point(39, 296);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.ImageLocation = "";
+            this.pictureBox6.Location = new System.Drawing.Point(271, 296);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.ImageLocation = "";
+            this.pictureBox7.Location = new System.Drawing.Point(534, 296);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.ImageLocation = "";
+            this.pictureBox8.Location = new System.Drawing.Point(756, 296);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(161, 141);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(219, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1002, 610);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Login";
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,5 +183,14 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- project/prototype1/prototype1/Login.cs
+++ project/prototype1/prototype1/Login.cs
@@ -15,6 +15,8 @@ namespace prototype1
         public Login()
         {
             InitializeComponent();
+            button2.Visible = false;
+            button3.Visible = false;
             LoadImage();
         }
 
@@ -23,10 +25,38 @@ namespace prototype1
             pictureBox1.Load("https://ih0.redbubble.net/image.363915459.3586/flat,800x800,075,f.u2.jpg");
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
 
+            pictureBox2.Load("https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/68/e2/b3/68e2b301-a83a-7f8f-bd49-7b3397999e50/AppIcon-1x_U007emarketing-85-220-6.png/246x0w.jpg");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox3.Load("https://ih0.redbubble.net/image.363915459.3586/flat,800x800,075,f.u2.jpg");
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox4.Load("https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/68/e2/b3/68e2b301-a83a-7f8f-bd49-7b3397999e50/AppIcon-1x_U007emarketing-85-220-6.png/246x0w.jpg");
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox1.Load("https://ih0.redbubble.net/image.363915459.3586/flat,800x800,075,f.u2.jpg");
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox5.Load("https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/68/e2/b3/68e2b301-a83a-7f8f-bd49-7b3397999e50/AppIcon-1x_U007emarketing-85-220-6.png/246x0w.jpg");
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox6.Load("https://ih0.redbubble.net/image.363915459.3586/flat,800x800,075,f.u2.jpg");
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox7.Load("https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/68/e2/b3/68e2b301-a83a-7f8f-bd49-7b3397999e50/AppIcon-1x_U007emarketing-85-220-6.png/246x0w.jpg");
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox8.Load("https://ih0.redbubble.net/image.363915459.3586/flat,800x800,075,f.u2.jpg");
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            button2.Visible = true;
+            button3.Visible = true;
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
             MessageBox.Show("Glimlag");
         }
     }
